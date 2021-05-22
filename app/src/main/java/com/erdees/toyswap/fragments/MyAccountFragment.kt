24f23dc2fd3f566d8395c19a4e7b5b5e61a6db8c
michalguspiel <guspielmichal@gmail.com>
2021-05-22 10:00:44 +0300

    Working on MyAccountFragment.kt and its layout plus logic.

--- app/src/main/java/com/erdees/toyswap/fragments/MyAccountFragment.kt
+++ app/src/main/java/com/erdees/toyswap/fragments/MyAccountFragment.kt
@@ -5,39 +5,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.erdees.toyswap.activities.MainActivity
 import com.erdees.toyswap.databinding.FragmentMyAccountBinding
 import com.erdees.toyswap.fragments.dialogs.ChangeAddressDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 
 class MyAccountFragment : Fragment() {
 
-    private var _binding : FragmentMyAccountBinding? = null
+    private var _binding: FragmentMyAccountBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var db : FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentMyAccountBinding.inflate(inflater,container,false)
+        _binding = FragmentMyAccountBinding.inflate(inflater, container, false)
         val view = binding.root
 
         auth = Firebase.auth
+        db  = Firebase.firestore
+        binding.profilePic.setAvatar()
 
         binding.changeAddressBtn.setOnClickListener {
             val changeAddressDialog = ChangeAddressDialog.newInstance()
-            changeAddressDialog.show(parentFragmentManager,ChangeAddressDialog.TAG)
+            changeAddressDialog.show(parentFragmentManager, ChangeAddressDialog.TAG)
         }
 
         binding.logoutButton.setOnClickListener {
-        auth.signOut()
+            auth.signOut()
             restartActivity()
         }
 
@@ -49,9 +56,20 @@ class MyAccountFragment : Fragment() {
         const val TAG = "MyAccountFragment"
     }
 
-    private fun restartActivity(){
-        val mainActivity = Intent(context,MainActivity::class.java)
+    private fun restartActivity() {
+        val mainActivity = Intent(context, MainActivity::class.java)
         startActivity(mainActivity)
+    }
+
+    private fun ImageView.setAvatar(){
+    db.collection("users").document(auth.uid.toString()).get().addOnSuccessListener {
+        val thisUserProfilePictureUrl =  it["avatar"]
+        Glide.with(requireActivity())
+            .load(thisUserProfilePictureUrl)
+            .centerCrop()
+            .into(this)
+    }
+
     }
 
     override fun onDestroyView() {
