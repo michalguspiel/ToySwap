298a241de21765cb5d280d3a640d4cb07ed24299
michalguspiel <guspielmichal@gmail.com>
2021-06-05 09:59:51 +0300

    Started to implement add item layout.

--- app/src/main/java/com/erdees/toyswap/view/fragments/ChatFragment.kt
+++ app/src/main/java/com/erdees/toyswap/view/fragments/ChatFragment.kt
@@ -1,4 +1,16 @@
 package com.erdees.toyswap.view.fragments
 
-class ChatFragment {
+import androidx.fragment.app.Fragment
+import com.erdees.toyswap.databinding.FragmentChatBinding
+
+class ChatFragment : Fragment(){
+    private var _binding : FragmentChatBinding? = null
+    private val binding get() = _binding!!
+
+
+    companion object {
+        const val TAG = "ChatFragment"
+        fun newInstance() : ChatFragment = ChatFragment()
+    }
+
 }
