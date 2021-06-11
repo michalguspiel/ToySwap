5cb6cfd0256fedb8565a58297a91e119d6da6f58
michalguspiel <guspielmichal@gmail.com>
2021-06-11 20:36:22 +0300

    Buggy dragging.

--- app/src/main/java/com/erdees/toyswap/view/fragments/AddItemFragment.kt
+++ app/src/main/java/com/erdees/toyswap/view/fragments/AddItemFragment.kt
@@ -2,19 +2,20 @@ package com.erdees.toyswap.view.fragments
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ClipData
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.erdees.toyswap.Constants
 import com.erdees.toyswap.R
 import com.erdees.toyswap.Utils
+import com.erdees.toyswap.Utils.makeGone
 import com.erdees.toyswap.databinding.FragmentAddItemBinding
 import com.erdees.toyswap.databinding.PictureGridItemBinding
 import com.erdees.toyswap.model.models.item.ItemCategory
@@ -26,14 +27,14 @@ import com.theartofdev.edmodo.cropper.CropImage
 class AddItemFragment : Fragment() {
 
 
-    private var _binding : FragmentAddItemBinding? = null
+    private var _binding: FragmentAddItemBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var pickedCategory: ItemCategory
 
-    private lateinit var picturePreview : PicturePreviewDialog
+    private lateinit var picturePreview: PicturePreviewDialog
 
-    private lateinit var viewModel : AddItemFragmentViewModel
+    private lateinit var viewModel: AddItemFragmentViewModel
 
     private val chooseCategoryDialog by lazy {
         ChooseCategoryDialog.newInstance()
@@ -44,65 +45,31 @@ class AddItemFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAddItemBinding.inflate(inflater,container,false)
+        _binding = FragmentAddItemBinding.inflate(inflater, container, false)
 
         val view = binding.root
         viewModel = ViewModelProvider(this).get(AddItemFragmentViewModel::class.java)
 
 
-        viewModel.categoryLiveData.observe(viewLifecycleOwner,{
-         if(it != null){
-             Log.i(TAG,it.categoryName)
-             pickedCategory = it
-             binding.itemChosenCategory.text = it.categoryName
-             binding.itemChooseCategoryBtn.text = "Change category"
-         }
-            if(it == null){
+        viewModel.categoryLiveData.observe(viewLifecycleOwner, {
+            if (it != null) {
+                Log.i(TAG, it.categoryName)
+                pickedCategory = it
+                binding.itemChosenCategory.text = it.categoryName
+                binding.itemChooseCategoryBtn.text = "Change category"
+            }
+            if (it == null) {
                 binding.itemChosenCategory.text = ""
                 binding.itemChooseCategoryBtn.text = "Pick category"
             }
         })
 
-        viewModel.picturesLiveData.observe(viewLifecycleOwner,{ picList ->
-            binding.addItemPicturesLayout.removeAllViews()
-            for(eachPic in picList) {
-                val eachPictureCard =
-                    LayoutInflater.from(requireContext())
-                        .inflate(R.layout.picture_grid_item, null, false)
-                val thisBinding = PictureGridItemBinding.bind(eachPictureCard)
-                if (eachPic == picList.first()) thisBinding.gridItemHead.text = "Main picture"
-                    Glide.with(requireActivity())
-                        .load(eachPic)
-                        .into(thisBinding.gridItemPicture)
-
-                thisBinding.gridItemPicture.setOnTouchListener { v, event ->
-                    val action = event.action
-                    view.requestDisallowInterceptTouchEvent(true)
-                    Log.i(TAG,action.toString())
-                    when (action) {
-                        MotionEvent.ACTION_DOWN -> {
-                            binding.root.disableScrollView()
-                            picturePreview = PicturePreviewDialog.newInstance(eachPic)
-                            picturePreview.show(parentFragmentManager, TAG)
-                        }
-                        MotionEvent.ACTION_UP -> {
-                            binding.root.enableScrollView()
-                            picturePreview.dismiss()
-
-                        }
-                    }
-                        return@setOnTouchListener true
-                }
-
-                thisBinding.gridRemovePicBtn.setOnClickListener{
-                    viewModel.removePicture(eachPic)
-                }
-                binding.addItemPicturesLayout.addView(eachPictureCard)
-            }
+        viewModel.picturesLiveData.observe(viewLifecycleOwner, { picList ->
+            buildPictureGridLayout(picList)
         })
 
         binding.itemChooseCategoryBtn.setOnClickListener {
-            chooseCategoryDialog.show(childFragmentManager,ChooseCategoryDialog.TAG)
+            chooseCategoryDialog.show(childFragmentManager, ChooseCategoryDialog.TAG)
         }
 
         binding.itemAddPictureBtn.setOnClickListener {
@@ -130,7 +97,7 @@ class AddItemFragment : Fragment() {
         )
         if (requestCode == Constants.REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
             data.data?.let { uri ->
-                Utils.launchImageCrop(uri, requireContext(), this,450,600)
+                Utils.launchImageCrop(uri, requireContext(), this, 450, 600)
             }
         } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             val result = CropImage.getActivityResult(data)
@@ -140,6 +107,89 @@ class AddItemFragment : Fragment() {
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 result.error
             }
+        }
+    }
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    fun buildPictureGridLayout(picList : List<Uri>){
+        binding.addItemPicturesLayout.removeAllViews()
+        for (eachPic in picList) {
+            val eachPictureCard =
+                LayoutInflater.from(requireContext())
+                    .inflate(R.layout.picture_grid_item, null, false)
+            val thisBinding = PictureGridItemBinding.bind(eachPictureCard)
+            if (eachPic == picList.first()) thisBinding.gridItemHead.text = "Main picture"
+            Glide.with(requireActivity())
+                .load(eachPic)
+                .into(thisBinding.gridItemPicture)
+
+            thisBinding.gridItemPicture.setOnTouchListener { v, event ->
+                val action = event.action
+                binding.root.requestDisallowInterceptTouchEvent(true)
+                Log.i(TAG, action.toString())
+                when (action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        binding.root.disableScrollView()
+                        picturePreview = PicturePreviewDialog.newInstance(eachPic)
+                        picturePreview.show(parentFragmentManager, TAG)
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        binding.root.enableScrollView()
+                        picturePreview.dismiss()
+
+                    }
+                }
+                return@setOnTouchListener true
+            }
+
+            thisBinding.gridItemLayout.setOnTouchListener { view, event ->
+                val action = event.action
+                when (action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        val data = ClipData.newPlainText("", "")
+                        val shadowBuilder = View.DragShadowBuilder(view)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            view.startDragAndDrop(data, shadowBuilder, view, 0)
+                        } else {
+                            view.startDrag(data, shadowBuilder, view, 0)
+                        }
+                        view.makeGone()
+                        return@setOnTouchListener true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        buildPictureGridLayout(picList)
+                        return@setOnTouchListener true
+                    }
+                    else ->{
+                        buildPictureGridLayout(picList)
+                        return@setOnTouchListener false
+                    }
+                }
+            }
+
+            thisBinding.gridItemLayout.setOnDragListener { v, event ->
+                val action = event.action
+                when (action) {
+                    DragEvent.ACTION_DRAG_STARTED -> {
+                        Log.i(TAG, "START DRAGGIN")
+                    }
+                    DragEvent.ACTION_DRAG_ENDED -> {
+                        buildPictureGridLayout(picList)
+                        Log.i(TAG, "END DRAGGIN")
+                    }
+                    DragEvent.ACTION_DROP -> {
+                        buildPictureGridLayout(picList)
+                    }
+                }
+
+                return@setOnDragListener true
+            }
+
+            thisBinding.gridRemovePicBtn.setOnClickListener {
+                viewModel.removePicture(eachPic)
+            }
+            binding.addItemPicturesLayout.addView(eachPictureCard)
         }
     }
 
