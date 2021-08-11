4d7acb1822204fb2d5983e7914ed49dc061101ce
michalguspiel <guspielmichal@gmail.com>
2021-08-11 11:59:42 +0300

    Implemented ROOM DB with my own pre populated database.

--- app/src/main/java/com/erdees/toyswap/model/localDatabase/ItemCondition.kt
+++ app/src/main/java/com/erdees/toyswap/model/localDatabase/ItemCondition.kt
@@ -3,8 +3,8 @@ package com.erdees.toyswap.model.localDatabase
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "item_conditions")
 data class ItemCondition(
-    @PrimaryKey val id: Long,
+    @PrimaryKey val id: Int?,
     val name: String
 )
--- app/src/main/java/com/erdees/toyswap/model/localDatabase/ItemConditionDao.kt
+++ app/src/main/java/com/erdees/toyswap/model/localDatabase/ItemConditionDao.kt
@@ -1,17 +1,12 @@
 package com.erdees.toyswap.model.localDatabase
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface ItemConditionDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertItemCondition(itemCondition: ItemCondition)
-
-    @Query("SELECT * FROM ItemCondition ORDER BY id")
+    @Query("SELECT * FROM item_conditions ORDER BY id")
     fun getItemConditions(): List<ItemCondition>
 
 }
